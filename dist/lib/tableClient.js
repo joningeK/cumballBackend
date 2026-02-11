bf0e8becd4ccbe39cdf608dce5f25796b4866016
Kåre Kjetting <andersen.joninge@gmail.com>
2026-02-11 22:04:02 +0100

    removed dist folder

--- dist/lib/tableClient.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.tasksTable = void 0;
-const data_tables_1 = require("@azure/data-tables");
-const connectionString = process.env.AzureWebJobsStorage;
-exports.tasksTable = data_tables_1.TableClient.fromConnectionString(connectionString, "Tasks");
